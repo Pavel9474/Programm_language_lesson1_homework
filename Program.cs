8d60c593e02beb1ca64e523b8272899b09a2fa67
Pavel <pavel.sharagin.1994@gmail.com>
2022-09-16 19:54:10 +0500

    task4 completed

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
 ﻿// Task 4
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите число 1:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2:");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 3:");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (b > max)
+{
+   max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+Console.WriteLine($"max = {max}");
