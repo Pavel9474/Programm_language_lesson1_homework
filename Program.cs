d895b2ddec4192130c3c5ae88fb4e5abc022c949
Pavel <pavel.sharagin.1994@gmail.com>
2022-09-16 20:12:17 +0500

    Task 6 completed

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Task 6
+Console.WriteLine("Введите число:");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0)
+{
+   Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число нечетное");
+}
