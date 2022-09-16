f76f07fd1650396c32abbde4ee18ef48b58ea5fb
Pavel <pavel.sharagin.1994@gmail.com>
2022-09-16 19:01:24 +0500

    task 2 completed

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
 ﻿// Task 2
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите число 1:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2:");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine($"max = {a}");
+}
+else if(b > a)
+{
+    Console.WriteLine($"max = {b}");
+}
+else
+{
+    Console.WriteLine("Числа равны");
+}
+
