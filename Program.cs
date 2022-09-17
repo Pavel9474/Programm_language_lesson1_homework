9fe51e2307904bfe648ab9d05b5a9f365f77e9ee
Pavel <pavel.sharagin.1994@gmail.com>
2022-09-17 10:16:58 +0500

    Additional task 1 completed

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
 ﻿//Additional task 1
-
+Console.WriteLine("Введите число:");
+int a = Convert.ToInt32(Console.ReadLine());
+int N = a * -1;
+while (N < a-1)
+{
+   Console.Write($"{N+=1},"); 
+}
+Console.Write($"{N+1}");
