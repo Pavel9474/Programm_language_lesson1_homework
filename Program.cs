9956654f3fd25366b131eb5f646e9ad1c94c4abd
Pavel <pavel.sharagin.1994@gmail.com>
2022-09-17 10:56:29 +0500

    initial commit

--- Program.cs
+++ Program.cs
@@ -1,12 +1,1 @@
 ﻿// Additional task 2
-Console.WriteLine("Введите число:");
-int a = Convert.ToInt32(Console.ReadLine());
-string b = Convert.ToString(a);
-if ((a < 1000) && (a > 99))
-{
-   Console.WriteLine("Последняя цифра числа: "+b[2]); 
-}
-else
-{
-    Console.WriteLine("Введите трехзначное число");
-}
